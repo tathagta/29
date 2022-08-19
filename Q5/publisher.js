b92afe00b75860657289a87926fa499f0b3f4f9b
tathagta <111573830+tathagta@users.noreply.github.com>
2022-08-19 17:25:30 +0530

    Add files via upload

---
+++ Q5/publisher.js
@@ -0,0 +1,29 @@
+var fs = require('fs')
+var mqtt = require('mqtt')
+const client = mqtt.connect('mqtt://broker:1883/')
+
+var dataFile
+
+fs.readFile("./data.json", function(err, data) {
+    if(err) {
+        console.log("File does not exits!")
+    }
+    else {
+        dataFile = data
+    }
+})
+
+
+client.on("connect", function(res, err) {
+    if(err) {
+        console.log(err)
+    } else {
+        console.log("Successfully connected to broker")
+        function pub() {
+            client.publish("test", dataFile)
+            console.log("Published")
+        }
+        setInterval(pub, 5000)
+    }
+})
+
